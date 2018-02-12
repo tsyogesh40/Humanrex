3b58200b9a5444fbc37f8176f6065487992f3458
tsyogesh40 <tsyogesh40@gmail.com>
2018-02-12 10:42:00 +0530

    staff panel _ bootstrap & Jquery added

--- application/views/staffs/staffs.php
+++ application/views/staffs/staffs.php
@@ -1,15 +1,19 @@
-<?php
-echo "Staffs page!";
- ?>
- <?php echo $this->session->userdata('name'); ?>
- <?php echo $this->session->userdata('staff_id'); ?>
- <?php echo $this->session->userdata('username'); ?>
- <?php echo $this->session->userdata('priority'); ?>
- <?php echo $this->session->userdata('lastlogin'); ?>
- <?php echo $this->session->userdata('email'); ?>
+
  <html>
  <head>
    <title>Staff Panel</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/staffs/staffs.css">
+
+  <script src="<?=base_url()?>assets/staffs/staffs.js"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 
  </head>
  <body>
